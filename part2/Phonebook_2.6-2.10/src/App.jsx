fafbecea545b74c4a0a2d5ba0f8602da2b9cfe63
root <root@lenovo-thinkpad-e560.fritz.box>
2024-02-19 20:30:48 +0100

    Exercise 2.6

--- part2/Phonebook_2.6-2.10/src/App.jsx
+++ part2/Phonebook_2.6-2.10/src/App.jsx
@@ -1,24 +1,61 @@
 import { useState } from 'react'
 
+const SectionTitle = (props) => <h2>{props.title}</h2>
+
+const SubmitForm = (props) => {
+    return (
+        <form onSubmit={props.addContact}>
+            <div>
+            name: <input value={props.newName} onChange={props.handleContactChange}/>
+            </div>
+            <div>
+                <button type="submit">add</button>
+            </div>
+      </form>
+    )
+}
+
+
+
+const ContactList = (props) => {
+    return (
+        props.contacts.map((card)=>{
+            return (<div key={card.name} >{card.name}</div>)
+    })
+    )
+}
+
+
+
+
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
-  const [newName, setNewName] = useState('')
+    const [persons, setPersons] = useState([
+        { name: 'Arto Hellas' },
+        { name: 'Elvis Cops' }
+        ]) 
+    const [newName, setNewName] = useState('')
+
+    const addContact = (event) => {
+        event.preventDefault()
+        const newContactCard = {
+            name: newName
+        }
+        setPersons(persons.concat(newContactCard))
+        setNewName('')
+    }
+
+    const handleContactChange = (event) => {
+        setNewName(event.target.value);
+    }
 
   return (
     <div>
-      <h2>Phonebook</h2>
-      <form>
-        <div>
-          name: <input />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      ...
+        <SectionTitle title={"Phonebook"}/>
+        <SubmitForm addContact={addContact} 
+                    handleContactChange={handleContactChange}
+                    newName={newName}/>
+        <SectionTitle title={"Numbers"}/>
+        <ContactList contacts={persons}/>
     </div>
   )
 }
